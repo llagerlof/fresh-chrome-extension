1c26ea11f8f48b2abbdb251c96ec72357d78dae2
Lawrence Lagerlof <llagerlof@gmail.com>
2021-08-08 18:04:55 -0300

    Converted from Manifest V2 to Manifest V3

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-// event to run execute.js contents when extension's button is clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(tab.ib, {
-    file: 'execute.js'
-  });
-});
+// event to run execute.js content when extension's button is clicked
+chrome.action.onClicked.addListener(execScript);
+
+async function execScript() {
+  const tabId = await getTabId();
+  chrome.scripting.executeScript({
+    target: {tabId: tabId},
+    files: ['execute.js']
+  })
+}
+
+async function getTabId() {
+  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  return (tabs.length > 0) ? tabs[0].id : null;
+}
