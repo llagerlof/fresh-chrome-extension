ef39af3c69393594fd8810509e542ec6fd4f5668
Lawrence Lagerlof <llagerlof@gmail.com>
2024-09-06 08:56:18 -0300

    Added version to the extension button and div msg.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -10,8 +10,9 @@ document.getElementById('btnShowOrangeDiv').addEventListener('click', function()
 
 // this code will be executed when the button btnShowOrangeDiv is clicked
 function modifyDOM() {
+  const version = chrome.runtime.getManifest().version;
   const div = document.createElement('div');
-  div.textContent = 'DOM modified! (I am in "popup.js")';
+  div.textContent = `DOM modified by Fresh Chrome Extension boilerplate ${version} (I am in "popup.js")`;
   div.style.cssText =
     'background-color: orange; color: black; font-weight: bold; padding: 10px; width: 100%; box-sizing: border-box;';
   document.body.insertAdjacentElement('afterbegin', div);
